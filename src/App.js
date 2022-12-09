ce0a95adb803379cf785219cbf169a25dd7c2c44
Yu Wang <yuwangconductor@gmail.com>
2022-12-09 14:33:00 -0500

    distance api done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+const { point } = require("@turf/helpers");
+const distance = require("@turf/distance").default;
 
 function App() {
   const [test, setTest] = useState(null);
 
+  // console.log(result);
+
   useEffect(() => {
     axios.get("http://localhost:8080/api/events/13").then((response) => {
-      console.log(response.data.latitude);
-      console.log(response.data.longitude);
-
-      setTest(response.data);
+      let longitude = response.data.longitude;
+      let latitude = response.data.latitude;
+      const from = point([longitude, latitude]);
+      const to = point([-79.3901, 43.6469]);
+      const options = { units: "kilometers" };
+      const result = distance(from, to, options);
+      setTest(result);
     });
   }, []);
 
   return (
     <>
       <p>this is app</p>
-      {/* <p>{`${test.event_name}`}</p> */}
+      <p>{`${test}`}</p>
     </>
   );
 }
