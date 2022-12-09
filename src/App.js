d3c4e9150c77d6abf14e9e2a7c8abeec411dd1b6
Yu Wang <yuwangconductor@gmail.com>
2022-12-08 23:19:13 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
-  return <p>This is App</p>;
+  const [test, setTest] = useState(null);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/events/13").then((response) => {
+      console.log(response.data.latitude);
+      console.log(response.data.longitude);
+
+      setTest(response.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <p>this is app</p>
+      {/* <p>{`${test.event_name}`}</p> */}
+    </>
+  );
 }
 
 export default App;
