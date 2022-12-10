e2e541b0942e3b0b58fb7b52ddf9b242e6406d22
Yu Wang <yuwangconductor@gmail.com>
2022-12-10 16:23:44 -0500

    fix month cal

--- src/utils/calculations.js
+++ src/utils/calculations.js
@@ -3,7 +3,7 @@ const distance = require("@turf/distance").default;
 
 export function getCurrentDate() {
   const date = new Date().getDate();
-  const month = new Date().getMonth();
+  const month = new Date().getMonth() + 1;
   const year = new Date().getFullYear();
   return date + "/" + month + "/" + year;
 }
