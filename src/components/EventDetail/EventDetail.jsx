cbd89d3c8831ed625024341f8418827a8825edb3
Yu Wang <yuwangconductor@gmail.com>
2022-12-11 22:35:45 -0500

    still need to choose better fonts

--- src/components/EventDetail/EventDetail.jsx
+++ src/components/EventDetail/EventDetail.jsx
@@ -5,25 +5,43 @@ import "./EventDetail.scss"
 function EventDetail({name, image, genre, location, venue, description, organization, googleMap, date, time, ticketLink, genreId, venueId, organizationId}){
 
     return(
-        <>
-            <p>{name}</p>
-            <p>{image}</p>
-            <Link to={`/genre/${genreId}`}>
-            <p>{genre}</p>
-            </Link>
-            <p>{location}</p>
-            <Link to={`/venue/${venueId}`}>
-            <p>{venue}</p>
-            </Link>
-            <p>{description}</p>
-            <Link to={`/organization/${organizationId}`}>
-            <p>{organization}</p>
-            </Link>
-            <p>{googleMap}</p>
-            <p>{date}</p>
-            <p>{time}</p>
-            <p>{ticketLink}</p>
-        </>
+        <article className="event-detail">
+
+            <div className="event-detail__hero-container">
+                <img src={image} alt={name} className="event-detail__image"/>
+            </div>
+
+            <div className="event-detail__main">
+                <div className="event-detail__title-container">
+                    <p className="event-detail__title">{name}</p>
+                </div>
+                <div className="event-detail__datetime-container">
+                    <p className="event-detail__date">{date}</p>
+                    <p className="event-detail__time">{time}</p>
+                </div>
+            </div>
+            <div className="event-detail__description-container">
+                    <p className="event-detail__description">{description}</p>
+            </div>
+            <div className="event-detail__detail">
+                <p className="event-detail__detail-title">PLAN TO GO</p>
+                <div className="event-detail__detail-container">
+                    <Link to={`/genre/${genreId}`} className="event-detail__genre event-detail__hover">{genre}</Link>
+                    <Link to={`/organization/${organizationId}`} className="event-detail__text event-detail__hover">{organization}</Link>
+                    <p className="event-detail__text event-detail__hover">{location}</p>
+                    <Link to={`/venue/${venueId}`} className="event-detail__text event-detail__hover">{venue}</Link>
+
+                </div>
+                <div className="event-detail__link-container">
+                    <a href={ticketLink} className="event-detail__link">
+                        <p>Ticket</p>
+                    </a>
+                </div>
+            </div>
+            <div className="event-detail__map-container">
+                <iframe className="event-detail__map" src={`https://www.google.com/maps/embed?pb=${googleMap}`}></iframe>
+            </div>
+        </article>
     )
 }
 
